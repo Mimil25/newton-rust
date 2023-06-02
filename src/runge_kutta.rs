52fd8a24af81c41861797ebe457e12d634579d54
mimil <emilien.decoux@free.fr>
2023-06-02 17:37:58 +0200

    runge kutta seem correct

--- src/runge_kutta.rs
+++ src/runge_kutta.rs
@@ -1,83 +1,180 @@
 use crate::sim_base::*;
 
-#[derive(Debug)]
-struct RK4Object {
-    id: u32,
-    o: Object,
-    dp: Vec2,
-}
-
 pub struct RungeKutta4 {
-    data: Vec<RK4Object>,
+    data: Vec<Object>,
 }
 
 impl<I: Iterator<Item = Object>> From<I> for RungeKutta4 {
     fn from(value: I) -> Self {
         RungeKutta4 {
-            data: Vec::from_iter(value.enumerate().map(|o| {
-                RK4Object {
-                    id: o.0 as u32,
-                    o: o.1,
-                    dp: Vec2::zero(),
-                }
-            })),
+            data: Vec::from_iter(value),
         }
     }
 }
 
 impl RungeKutta4 {
-    fn acceleration(&self, a: &RK4Object) -> Vec2 {
+    fn acceleration(&self, a: &Object, id: usize) -> Vec2 {
         self.data.iter()
-            .filter(|b| {a.id != b.id})
-            .map(|b| {force_between(&a.o, &b.o)})
+            .enumerate()
+            .filter(|b| {id != b.0})
+            .map(|b| {force_between(&a, &b.1)})
             .sum()
     }
 }
 
 impl Simulator for RungeKutta4 {
     fn get_objects<'a>(&'a self) -> Box<(dyn Iterator<Item = &'a Object> + 'a)> {
-        Box::new(self.data.iter().map(|o| {
-            &o.o
-        }))
+        Box::new(self.data.iter())
     }
     fn len(&self) -> usize {
         self.data.len()
     }
     fn step(&mut self, dt: f32) {
         // http://spiff.rit.edu/richmond/nbody/OrbitRungeKutta4.pdf
-        for i in 0..self.data.len() {
-            let a = &self.data[i];
-            let k1p = a.o.v;
-            let k1v : Vec2 = self.acceleration(a);
 
-            let k2p = a.o.v + k1v*(dt*0.5);
-            let mut tmp = RK4Object {
-                id: a.id,
-                o:Object {
-                    p: a.o.p + k1p * (dt*0.5),
-                    v: Vec2::zero(), // not used
-                    m: a.o.m,
-                    r: a.o.r,
-                },
-                dp: Vec2::zero(), // not used
-            };
-            let k2v : Vec2 = self.acceleration(&tmp);
-            let k3p = a.o.v + k2v*(dt*0.5);
-            tmp.o.p = a.o.p + k2p * (dt*0.5);
-            let k3v : Vec2 = self.acceleration(&tmp);
+        let k1p: Vec<Vec2> = self.data.iter().map(|o| {o.v}).collect();
+        let k1v: Vec<Vec2> = self.data.iter()
+            .enumerate()
+            .map(|(id, o)| {
+                self.acceleration(o, id)
+            })
+            .collect();
 
-            let k4p = a.o.v + k3v * dt;
-            tmp.o.p = a.o.p + k3p * dt;
-            let k4v : Vec2 = self.acceleration(&tmp);
+        let ddt = dt * 0.5;
+        let k2p: Vec<Vec2> = k1p.iter()
+            .zip(k1v.iter())
+            .map(|(v, k1v)| {
+                *v + *k1v * ddt
+            })
+            .collect();
+        let k2v: Vec<Vec2> = self.data.iter()
+            .zip(k1p.iter())
+            .zip(k1v.iter())
+            .map(|((o, k1p), k1v)| {
+                Object {
+                    p: o.p + *k1p * ddt,
+                    v: o.v + *k1v * ddt,
+                    m: o.m,
+                    r: o.r,
+                }
+            })
+            .enumerate()
+            .map(|(id, o)| {
+                self.data.iter()
+                    .zip(k1p.iter())
+                    .zip(k1v.iter())
+                    .enumerate()
+                    .filter(|(id2, _)| {id != *id2})
+                    .map(|(_id, ((o, k1p), k1v))| {
+                        Object {
+                            p: o.p + *k1p * ddt,
+                            v: o.v + *k1v * ddt,
+                            m: o.m,
+                            r: o.r,
+                        }
+                    })
+                    .map(|b| {
+                        force_between(&o, &b)
+                    })
+                    .sum::<Vec2>()
+            })
+            .collect();
 
-            let m = a.o.m;
-            self.data[i].o.v += (k1v + k2v+k2v + k3v+k3v + k4v) * (dt / (6.*m));
-            self.data[i].dp = (k1p + k2p+k2p + k3p+k3p + k4p) * (dt / 6.);
+        let k3p: Vec<Vec2> = k2p.iter()
+            .zip(k2v.iter())
+            .map(|(v, k2v)| {
+                *v + *k2v * ddt
+            })
+            .collect();
+        let k3v: Vec<Vec2> = self.data.iter()
+            .zip(k2p.iter())
+            .zip(k2v.iter())
+            .map(|((o, k2p), k2v)| {
+                Object {
+                    p: o.p + *k2p * ddt,
+                    v: o.v + *k2v * ddt,
+                    m: o.m,
+                    r: o.r,
+                }
+            })
+            .enumerate()
+            .map(|(id, o)| {
+                self.data.iter()
+                    .zip(k2p.iter())
+                    .zip(k2v.iter())
+                    .enumerate()
+                    .filter(|(id2, _)| {id != *id2})
+                    .map(|(_id, ((o, k2p), k2v))| {
+                        Object {
+                            p: o.p + *k2p * ddt,
+                            v: o.v + *k2v * ddt,
+                            m: o.m,
+                            r: o.r,
+                        }
+                    })
+                    .map(|b| {
+                        force_between(&o, &b)
+                    })
+                    .sum::<Vec2>()
+            })
+            .collect();
 
-        }
+        let k4p: Vec<Vec2> = k3p.iter()
+            .zip(k3v.iter())
+            .map(|(v, k3v)| {
+                *v + *k3v * dt
+            })
+            .collect();
+        let k4v: Vec<Vec2> = self.data.iter()
+            .zip(k3p.iter())
+            .zip(k3v.iter())
+            .map(|((o, k3p), k3v)| {
+                Object {
+                    p: o.p + *k3p * dt,
+                    v: o.v + *k3v * dt,
+                    m: o.m,
+                    r: o.r,
+                }
+            })
+            .enumerate()
+            .map(|(id, o)| {
+                self.data.iter()
+                    .zip(k3p.iter())
+                    .zip(k3v.iter())
+                    .enumerate()
+                    .filter(|(id2, _)| {id != *id2})
+                    .map(|(_id, ((o, k3p), k3v))| {
+                        Object {
+                            p: o.p + *k3p * dt,
+                            v: o.v + *k3v * dt,
+                            m: o.m,
+                            r: o.r,
+                        }
+                    })
+                    .map(|b| {
+                        force_between(&o, &b)
+                    })
+                    .sum::<Vec2>()
+            })
+            .collect();
 
-        for o in self.data.iter_mut() {
-            o.o.p += o.dp;
+        let dp = k1p.into_iter()
+            .zip(k2p)
+            .zip(k3p)
+            .zip(k4p)
+            .map(|(((k1, k2), k3), k4)| {
+                (k1 + k2+k2 + k3+k3 + k4) * (dt/6.)
+            });
+        let dv = k1v.into_iter()
+            .zip(k2v)
+            .zip(k3v)
+            .zip(k4v)
+            .map(|(((k1, k2), k3), k4)| {
+                (k1 + k2+k2 + k3+k3 + k4) * (dt/6.)
+            });
+        for ((o, dp), dv) in self.data.iter_mut().zip(dp).zip(dv) {
+            o.p += dp;
+            o.v += dv * (1./o.m);
         }
     }
 }
